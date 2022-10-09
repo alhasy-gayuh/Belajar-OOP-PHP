97ab6d791b534ffe9a8ab58596acfa31b7ce9bfd
alhasy-gayuh <alhasygayuh@gmail.com>
2022-10-09 20:07:40 +0700

    membuat property dan method

--- README.md
+++ README.md
@@ -6,3 +6,20 @@ Keunggulan OOP PHP:
 1. dapat menulis lebih ringkas dan cepat
 2. mudah di review kembali di kemudian hari
 3. struktur lebih terlihat jelas di mata program.
+
+di OOP PHP ada beberapa Structural
+1. Class
+2. Object
+3. Property
+4. Method
+5. Construct
+6. Inheritance
+7. Setter & Getter, dsb.
+
+# Class dan Object
+jika di ibaratkan buah buahan.
+Buah adalah sebuah Class.
+Apel, Mangga, Jeruk adalah sebuah Objek.
+
+warna dan bentuk dari setiap buah buahan di namakan property
+lalu perilaku dari buah tersebut namanya method. contoh: apel dapat dimakan, memiliki rasa yang manis, dsb.
--- produk.php
+++ produk.php
@@ -1,5 +1,27 @@
 <?php
-class Produk{
+class Produk{ // >> ini namanya class
+    public $nama, $penjual, $pembeli, $harga; // >> ini property
+
+    public function getLabel(){ // >> ini adalah method
+        return "$this->nama $this->penjual"; // >> Memanggil property di luar method menggunakan this
+        // jika this ada di dalam class maka akan mengacu ke pada class, jika di luar class maka akan mengacu kepada property global
+    }
 
 }
-$produk = new Produk();
+$produk1 = new Produk(); // >> ini disebut object
+$produk2 = new Produk();
+
+$produk1->nama = 'Surya'; // >> mengisi nilai dalam property
+$produk1->penjual = 'Gayuh'; // memasukkan nilai ke dalam property lalu property di masukkan ke dalam object
+$produk1->pembeli = 'Anisa';
+$produk1->harga = 40000;
+
+$produk2->nama = 'Kopi'; // >> mengisi nilai dalam property
+$produk2->penjual = 'Fajar'; // memasukkan nilai ke dalam property lalu property di masukkan ke dalam object
+$produk2->pembeli = 'Taufik';
+$produk2->harga = 34220;
+
+echo "Data Penjualan : Nama Produk $produk1->nama, Nama Pembeli $produk1->penjual";
+echo "<br>";
+echo "<br>";
+echo $produk2->getLabel();
