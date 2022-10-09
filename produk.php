3bdf582781c4fdc4405aa41a0b66decd184326bf
alhasy-gayuh <alhasygayuh@gmail.com>
2022-10-09 20:25:47 +0700

    Menambahkan Construct

--- produk.php
+++ produk.php
@@ -7,19 +7,24 @@ class Produk{ // >> ini namanya class
         // jika this ada di dalam class maka akan mengacu ke pada class, jika di luar class maka akan mengacu kepada property global
     }
 
-}
-$produk1 = new Produk(); // >> ini disebut object
-$produk2 = new Produk();
+    public function __construct($nama, $penjual, $pembeli, $harga)
+    {
+        $this->nama = $nama;
+        $this->penjual = $penjual;
+        $this->pembeli = $pembeli;
+        $this->harga = $harga;
 
+    }
+
+}
+$produk2 = new Produk("Sampoerna","Alhasy","Anisa",3452); // menggunakan construct lebih sederhana
+echo $produk2->getLabel();
+
+// $produk1 = new Produk(); // >> ini disebut object
 $produk1->nama = 'Surya'; // >> mengisi nilai dalam property
 $produk1->penjual = 'Gayuh'; // memasukkan nilai ke dalam property lalu property di masukkan ke dalam object
 $produk1->pembeli = 'Anisa';
 $produk1->harga = 40000;
-
-$produk2->nama = 'Kopi'; // >> mengisi nilai dalam property
-$produk2->penjual = 'Fajar'; // memasukkan nilai ke dalam property lalu property di masukkan ke dalam object
-$produk2->pembeli = 'Taufik';
-$produk2->harga = 34220;
 
 echo "Data Penjualan : Nama Produk $produk1->nama, Nama Pembeli $produk1->penjual";
 echo "<br>";
